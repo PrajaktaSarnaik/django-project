4999606e54653b2b15334d99e09ee88a96ecfdd5
Prajakta Sarnaik <prajsarnaik@gmail.com>
2025-01-14 10:59:36 +0000

    Add HTTP request in hello_world views.py

--- my_project/urls.py
+++ my_project/urls.py
@@ -20,8 +20,8 @@ from hello_world import views as index_views
 from about import views as about_views
 
 urlpatterns = [
-    # path('', index_views.index, name='index'),  # Add this line to handle the root path
-    path('hello/', index_views.index, name='index'),
+    path('', index_views.index, name='index'),  # Add this line to handle the root path
+    # path('hello/', index_views.index, name='index'),
     path('about/', about_views.about_me, name='index'),
     path('admin/', admin.site.urls),
 ]
