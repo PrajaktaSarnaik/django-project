c022d948f55538a88684c7d24569004c05f8556c
Prajakta Sarnaik <prajsarnaik@gmail.com>
2025-01-14 11:28:43 +0000

    Make a change to see the detailed output

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(rn3%xnv==p3zw%+r-c0lx$8vm!wiivrzk6nv$aau(m7#sql_7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-nielmc-django-project-0kylrta3cs.us2.codeanyapp.com','.herokuapp.com']
 
